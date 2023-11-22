b8af61ad08ee9a65b8b0d66186ae7e7352e2d8f9
yuto-masuda <yuto.masuda.0719@gmail.com>
2023-11-22 22:00:27 +0900

    位置情報を受け取る、半径1.5km内のspots取得

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::SpotsController < ApplicationController
   def index
+    lat1 = params[:lat].to_f
+    lng1 = params[:lng].to_f
+    # lat1 = 35.5505809
+    # lng1 = 139.4455242
     @spots = Spot.all
-    render json: @spots
+    @spots_within_radius = @spots.select do |spot|
+      spot.within_radius?(lat1, lng1, spot.latitude, spot.longitude)
+    end
+    render json: @spots_within_radius
   end
 
   def show
