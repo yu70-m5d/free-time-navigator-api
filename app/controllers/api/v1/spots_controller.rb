64e6ac63fd2b36d21bdc208b7fc5323afcaeae30
yuto-masuda <yuto.masuda.0719@gmail.com>
2024-01-26 15:19:07 +0900

    近い順に表示

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::SpotsController < ApplicationController
     lng1 = params[:lng].to_f
 
     spots = Spot.within_radius(lat1, lng1).includes(:tags)
+    spots = sort_by_near_distance(spots, lat1, lng1)
     spots_hash = spots.as_json(include: :tags)
 
     if params[:time].present?
@@ -30,6 +31,7 @@ class Api::V1::SpotsController < ApplicationController
     lng1 = params[:lng].to_f
 
     spots = Spot.within_radius(lat1, lng1).includes(:tags)
+    spots = sort_by_near_distance(spots, lat1, lng1)
     spots_hash = spots.as_json(include: :tags)
 
     if params[:time].present?
@@ -53,6 +55,37 @@ class Api::V1::SpotsController < ApplicationController
   def show
     @spot = Spot.find(params[:id]).as_json(include: [:tags])
     render json: @spot
+  end
+
+  private
+
+  def sort_by_near_distance(spots, lat, lng)
+    spots.sort_by do |spot|
+      # distance_squared(lat, lng, spot.latitude, spot.longitude)
+      haversine_distance(lat, lng, spot.latitude, spot.longitude)
+    end
+  end
+
+  # def distance_squared(lat1, lng1, lat2, lng2)
+  #   (lat2 - lat1)**2 + (lng2 - lng1)**2
+  # end
+
+  def to_radians(degrees)
+    degrees * Math::PI / 180.0
+  end
+
+  def haversine_distance(lat1, lng1, lat2, lng2)
+    dlat = to_radians( lat2 - lat1 )
+    dlng = to_radians( lng2 - lng1 )
+
+    a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+        Math.cos(to_radians(lat1)) * Math.cos(to_radians(lat2)) *
+        Math.sin(dlng / 2) * Math.sin(dlng / 2)
+
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    r = 6371
+
+    c * r
   end
 
 end
