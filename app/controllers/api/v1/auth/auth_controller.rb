bb932e87ec71100afb610898d6afaab2719d03d5
yuto-masuda <yuto.masuda.0719@gmail.com>
2024-03-07 21:18:19 +0900

    環境変数修正

--- app/controllers/api/v1/auth/auth_controller.rb
+++ app/controllers/api/v1/auth/auth_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::Auth::AuthController < ApplicationController
   def line_url
-    client_id = ENV['LINE_KEY']
-    redirect_uri = 'https://25db-2001-268-c20e-1490-d4a2-84e0-dfd6-6a09.ngrok-free.app/auth/line/callback'
+    client_id = Rails.application.credentials.line&.fetch(:line_id)
+    development_uri = 'https://3f05-2001-268-c204-1b09-495e-8686-fb22-ce6d.ngrok-free.app/auth/line/callback'
+    production_uri = 'https://free-time-navigator-api.onrender.com/auth/line/callback'
     state = SecureRandom.hex(10) # CSRF対策のためにランダムな状態を生成
     scope = 'profile openid' # 必要に応じてスコープを設定
+
+    Rails.env.production? ? redirect_uri = production_uri : redirect_uri = development_uri
+
 
     line_auth_url = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=#{client_id}&redirect_uri=#{redirect_uri}&state=#{state}&scope=#{scope}"
     render json: { line_auth_url: line_auth_url }
--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -2,17 +2,23 @@ class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
 
   def create
     user_id = sign_in_params[:user_id]
-    @resource = User.find_by(id: user_id)
-    if @resource
-      @token_data = @resource.create_token
-      @resource.save!
-      render json: { userId: @resource.id, client: @token_data.client, token: @token_data.token, uid: @resource.uid }
+    user_email = sign_in_params[:email]
+
+    if user_id
+      @resource = User.find_by(id: user_id)
+      if @resource
+        @token_data = @resource.create_token
+        @resource.save!
+        render json: { userId: @resource.id, client: @token_data.client, token: @token_data.token, uid: @resource.uid }
+      end
+    else
+      super
     end
   end
 
   private
 
   def sign_in_params
-    params.require(:session).permit(:user_id)
+    params.permit(:user_id, :email, :password)
   end
 end
