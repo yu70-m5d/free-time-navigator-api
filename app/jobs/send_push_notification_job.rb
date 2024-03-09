752926c17f957b8a4f81ae45fa4420c7d6205174
yuto-masuda <yuto.masuda.0719@gmail.com>
2024-03-09 19:32:46 +0900

    timeテスト

--- app/jobs/send_push_notification_job.rb
+++ app/jobs/send_push_notification_job.rb
@@ -5,15 +5,17 @@ class SendPushNotificationJob < ApplicationJob
 
   def perform(uid, time)
     # Do something later
-    client ||= Line::Bot::Client.new { |config|
+    client ||= Line::Bot::Client.new do |config|
       config.channel_id = Rails.application.credentials.line&.fetch(:line_messaging_id)
       config.channel_secret = Rails.application.credentials.line&.fetch(:line_messaging_secret)
       config.channel_token = Rails.application.credentials.line&.fetch(:line_messaging_access_token)
-  }
+    end
+
+    now_time = Time.now
 
     message = {
       type: 'text',
-      text: "#{time}です。通知を設定した時刻になりました！"
+      text: "#{now_time}です。通知を設定した時刻になりました！"
     }
 
     client.push_message(uid, message)
